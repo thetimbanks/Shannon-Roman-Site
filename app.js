52146b266d7270fead91228e237f8eeee1fcc75d
Tim Banks <thetimbanks@gmail.com>
2012-03-11 00:28:19 -0600

    Updated the listening port to look for the environment variable first and then fall back to 3000 for development

--- app.js
+++ app.js
@@ -66,5 +66,6 @@ app.dynamicHelpers({
 // Routes
 app.get('/', routes.index);
 
-app.listen(42222);
+var port = process.env.PORT || 3000;
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
