057fa537cfb3affb88d951d8eb44496ef03726e4
Tim Banks <thetimbanks@gmail.com>
2012-03-11 00:23:59 -0600

    Updated the listening port to 42222

--- app.js
+++ app.js
@@ -66,5 +66,5 @@ app.dynamicHelpers({
 // Routes
 app.get('/', routes.index);
 
-app.listen(3000);
+app.listen(42222);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
